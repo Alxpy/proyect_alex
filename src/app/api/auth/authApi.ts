a4f537dfa4ff53ebd234f6fe22d6a06474d0710f
alxpy <alxpyk09@gmail.com>
2025-04-16 00:58:40 -0400

    add RegistertApi function for user registration

--- src/app/api/auth/authApi.ts
+++ src/app/api/auth/authApi.ts
@@ -81,3 +81,42 @@ export const logoutApi = async () => {
     }
   }
 };
+
+export const RegistertApi = async () => {
+  try {
+    const res = await axios.put(`/register`);
+
+    const body = res.data;
+    if (res.status === 200) {
+      return {
+        success: true,
+        message: body.message,
+        status: body.status,
+        data: body.data,
+      };
+    } else {
+      return {
+        success: false,
+        message: body.message,
+        status: body.status,
+        data: null,
+      };
+    }
+  } catch (err: any) {
+    if (err.response) {
+      return {
+        success: false,
+        message: err.response.data.message,
+        status: err.response.data.status,
+        data: null,
+      };
+    } else {
+      return {
+        success: false,
+        message: err.message || "Opps! Algo salió mal, intente más tarde.",
+        status: "error",
+        data: null,
+      };
+    }
+  }
+};
